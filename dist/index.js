b24b73421cf53d92837f9d6dc4fb9ce632a229fc
Abdul Rehman <114314272+Maani5790@users.noreply.github.com>
2023-01-30 01:57:04 +0500

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let arr1 = [1, 3, 4, 57, 8, 9];
+let arr2 = ['maani', 'rehman', 2];
+arr2.push(2);
+console.log(arr2);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+// console.log("hello world")
+
+// let age : number = 20;
+// if(age < 50){
+//     age += 10;
+//     console.log(age);
+
+// }
+
+// let names:string = 'rehman';
+// names = "maani";
+// console.log(names);
+
+// let level;
+// level = 23;
+// level = "maani"
+
+
+                        //    Array  //
+
+
+// let arr1: number[] = [1,3,4,57,8,9];
+
+
+                                // Tuple  //
+
+// let arr2 : [string,string,number] = ['maani', 'rehman',2];
+// arr2.push(2);
+// console.log(arr2);
+
+
+
+
+
+
+
